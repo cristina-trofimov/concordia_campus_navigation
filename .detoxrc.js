19e9c86eec8bcf21b53fa356d99830c036cfa2a1
Nao <naolal30@hotmail.com>
2025-01-29 12:21:00 -0500

    fix detox?

--- .detoxrc.js
+++ .detoxrc.js
@@ -10,16 +10,6 @@ module.exports = {
     }
   },
   apps: {
-    'ios.debug': {
-      type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/concordia_campus_navigation.app',
-      build: 'xcodebuild -workspace ios/concordia_campus_navigation.xcworkspace -scheme concordia_campus_navigation -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
-    },
-    'ios.release': {
-      type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/concordia_campus_navigation.app',
-      build: 'xcodebuild -workspace ios/concordia_campus_navigation.xcworkspace -scheme concordia_campus_navigation -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
-    },
     'android.debug': {
       type: 'android.apk',
       binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
@@ -35,12 +25,6 @@ module.exports = {
     }
   },
   devices: {
-    simulator: {
-      type: 'ios.simulator',
-      device: {
-        type: 'iPhone 15'
-      }
-    },
     attached: {
       type: 'android.attached',
       device: {
@@ -55,14 +39,6 @@ module.exports = {
     }
   },
   configurations: {
-    'ios.sim.debug': {
-      device: 'simulator',
-      app: 'ios.debug'
-    },
-    'ios.sim.release': {
-      device: 'simulator',
-      app: 'ios.release'
-    },
     'android.att.debug': {
       device: 'attached',
       app: 'android.debug'
