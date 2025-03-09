2f33c7122536135776ae65739614a2404ac6967b
Julia Trinh <juliatrinh2002@gmail.com>
2025-03-08 21:11:31 -0500

    geojson of H4 and H5

---
+++ src/interfaces/roomProperties.ts
@@ -0,0 +1,12 @@
+export interface RoomProperties {
+    level: string;
+    indoor?: string;
+    ref?: string;
+    entrance?: string;
+    highway?: string;
+    name?: string;
+    amenity?: string;
+    male?: string;
+    female?: string;
+    escalators?: string;
+}
