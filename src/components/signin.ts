c6fd5d4346476d6a2a33860e17170c9b05f01fb3
j-ackamir <ja_amir@live.concordia.ca>
2025-03-18 17:05:27 -0400

    Remove unnecessary comments

--- src/components/signin.ts
+++ src/components/signin.ts
@@ -1,26 +1,24 @@
-// import statusCodes along with GoogleSignin
 import {
     GoogleSignin,
     isErrorWithCode,
     statusCodes,
 } from '@react-native-google-signin/google-signin';
 
-// Somewhere in your code
 export const signIn = async () => {
     try {
-        // First check if user is already signed in
+        
         const currentUser = await GoogleSignin.getCurrentUser();
         if (currentUser) {
-            // User is already signed in, we can get current user
+            
             const currentUser = await GoogleSignin.getCurrentUser();
             console.log("Already signed in user:", JSON.stringify(currentUser, null, 2));
             return currentUser;
         }
 
-        // Make sure Play Services are available
+        
         await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
 
-        // Attempt sign in
+        
         const userInfo = await GoogleSignin.signIn();
         console.log("Sign-in successful. User Info:", JSON.stringify(userInfo, null, 2));
 
--- src/components/signout.ts
+++ src/components/signout.ts
@@ -4,7 +4,7 @@ export const signOut = async () => {
     try {
         await GoogleSignin.signOut();
         console.log('User signed out successfully');
-        // Update your app's state here (e.g., clear user data, navigate to login screen)
+        
     } catch (error) {
         console.error('Error signing out:', error);
     }
