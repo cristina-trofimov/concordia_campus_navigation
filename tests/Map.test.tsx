41b9bf0a4793a56db2bce1b2f11c9661f063db3e
Otaphoque <knztnce@gmail.com>
2025-03-20 22:32:43 -0400

    root dir issue fixed

--- tests/Map.test.tsx
+++ tests/Map.test.tsx
@@ -1,6 +1,35 @@
+// At the very top of your Map.test.tsx file
+jest.mock('../src/components/ShuttleBusTracker.tsx', () => 'ShuttleBusTracker', { virtual: true });
+
 import React from 'react';
 import { render } from '@testing-library/react-native';
 import { Animated } from 'react-native';
+
+jest.mock('../src/components/BuildingCoordinates', () => ({
+  HighlightBuilding: (props) => {
+    // Simple mock implementation that doesn't call setIsInsideBuilding
+    return null;
+  }
+}));
+
+jest.mock('../src/data/CoordsContext.tsx', () => ({
+  useCoords: jest.fn().mockReturnValue({
+    routeData: [],
+    setmyLocationString: jest.fn(),
+    myLocationString: '',
+    setIsInsideBuilding: jest.fn(), // Add this function
+    isInsideBuilding: false         // Add this state value
+  })
+}));
+
+// Add this mock if there's a separate BuildingContext
+jest.mock('../src/data/CoordsContext.tsx', () => ({
+  useBuildingContext: jest.fn().mockReturnValue({
+    setIsInsideBuilding: jest.fn(),
+    isInsideBuilding: false
+  })
+}));
+
 import Map from '../src/components/Map.tsx';
 import * as Location from 'expo-location';
 
