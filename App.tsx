72b1afccf92fd8478a9a7437036a0db3fdb3eaf9
NAO <Naolal30@hotmail.com>
2025-03-22 00:18:28 -0400

    works

--- App.tsx
+++ App.tsx
@@ -25,11 +25,23 @@ const theme = createTheme({
 });
 
 export default function App(): React.ReactElement {
+      const [isTesting, setIsTesting] = useState(false);
 useEffect(() => {
-    analytics().setAnalyticsCollectionEnabled(true);
-    crashlytics().setCrashlyticsCollectionEnabled(true);
-    analytics().logAppOpen();
-  }, []);
+  console.log('Initializing Firebase...');
+  analytics().setAnalyticsCollectionEnabled(true);
+  crashlytics().setCrashlyticsCollectionEnabled(true);
+  analytics().logAppOpen();
+
+ if (isTesting) {
+    analytics().logEvent('testing_mode_enabled', {
+      message: 'App is in testing mode.',
+    });
+    console.log('Custom Event Triggered: testing_mode_enabled');
+    console.log('Event Data:', {
+      message: 'App is in testing mode.',
+    });
+  }
+}, [isTesting]);
 
   return (
     <NavigationContainer>
