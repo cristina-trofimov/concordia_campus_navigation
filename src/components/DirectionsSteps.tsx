1e283f7512220ba1715f250fbbf82190526d1f6f
Midhurshaan <midhur-2002@hotmail.com>
2025-03-23 23:27:46 -0400

    moved the instruction formatting to a method in order to remove duplicate code

--- src/components/DirectionsSteps.tsx
+++ src/components/DirectionsSteps.tsx
@@ -4,19 +4,20 @@ import { useCoords } from "../data/CoordsContext";
 import { DirectionStepsStyle } from "../styles/DirectionsStepsStyle";
 import { MaterialIcons } from "@expo/vector-icons";
 
-  const DirectionsSteps = () => {
+const DirectionsSteps = () => {
+  const { routeData: routeCoordinates, isTransit } = useCoords();
+  const [htmlInstructions, setHtmlInstructions] = useState<string[]>([]);
 
-    const { routeData: routeCoordinates,isTransit} = useCoords();
-    const [htmlInstructions, setHtmlInstructions] = useState<string[]>([]);
+  const formatSteps = (step: any): any => {
+    return step.html_instructions
+      .replace(/<[^<>]*>/g, "")
+      .replace(/(?=Destination)/gi, ". ");
+  };
 
   useEffect(() => {
     if (routeCoordinates && routeCoordinates.length > 0) {
-      const instructions = routeCoordinates[0].legs[0].steps.map(
-        (step: any) => {
-          return step.html_instructions
-            .replace(/<[^<>]*>/g, "")
-            .replace(/(?=Destination)/gi, ". ");
-        }
+      const instructions = routeCoordinates[0].legs[0].steps.map((step: any) =>
+        formatSteps(step)
       );
 
       let detailedInstructions: string[] = [];
@@ -29,11 +30,7 @@ import { MaterialIcons } from "@expo/vector-icons";
       for (let i = 0; i < number; i++) {
         const detailedHtmlInstructions = routeCoordinates[0]?.legs[0]?.steps[
           i
-        ]?.steps?.map((step: any) => {
-          return step.html_instructions
-            .replace(/<[^<>]*>/g, "")
-            .replace(/(?=Destination)/gi, ". ");
-        });
+        ]?.steps?.map((step: any) => formatSteps(step));
         if (detailedHtmlInstructions == undefined) {
           detailedInstructions = detailedInstructions.concat(
             instructionsGeneral[i]
@@ -48,69 +45,78 @@ import { MaterialIcons } from "@expo/vector-icons";
         }
       }
 
-      isTransit ? setHtmlInstructions(detailedInstructions) : setHtmlInstructions(instructions);
+      isTransit
+        ? setHtmlInstructions(detailedInstructions)
+        : setHtmlInstructions(instructions);
 
-    // console.log(htmlInstructions);
+      // console.log(htmlInstructions);
     } else {
       setHtmlInstructions([]);
     }
   }, [routeCoordinates]);
 
-  return(
-      <View style={ DirectionStepsStyle.container}>
+  return (
+    <View style={DirectionStepsStyle.container}>
       <ScrollView>
-      {htmlInstructions.length > 0 &&
-        htmlInstructions.map((instruction, index) => {
-          const iconMappings: { [key: string]: string } = {
-            destination: 'location-on',
-            left: 'turn-left',
-            right: 'turn-right',
-            walk: 'directions-walk',
-            bus: 'directions-bus',
-            metro: 'directions-subway',
-            subway: 'directions-subway',
-            train: 'directions-train',
-            merge: 'merge',
-            straight: 'straight',
-            continue: 'straight',
-            northeast: 'turn-slight-right',
-            northwest: 'turn-slight-left',
-            southeast: 'turn-slight-right',
-            southwest: 'turn-slight-left',
-            exit: 'arrow-outward',
-            wait: 'access-time'
-          };
+        {htmlInstructions.length > 0 &&
+          htmlInstructions.map((instruction, index) => {
+            const iconMappings: { [key: string]: string } = {
+              destination: "location-on",
+              left: "turn-left",
+              right: "turn-right",
+              walk: "directions-walk",
+              bus: "directions-bus",
+              metro: "directions-subway",
+              subway: "directions-subway",
+              train: "directions-train",
+              merge: "merge",
+              straight: "straight",
+              continue: "straight",
+              northeast: "turn-slight-right",
+              northwest: "turn-slight-left",
+              southeast: "turn-slight-right",
+              southwest: "turn-slight-left",
+              exit: "arrow-outward",
+              wait: "access-time",
+            };
 
-          const lowerCaseInstruction = instruction.toLowerCase();
+            const lowerCaseInstruction = instruction.toLowerCase();
 
-          const instructionsIconsDisplay = Object.keys(iconMappings).find((key) => lowerCaseInstruction.includes(key))
-              ? iconMappings[Object.keys(iconMappings).find((key) => lowerCaseInstruction.includes(key))!]
+            const instructionsIconsDisplay = Object.keys(iconMappings).find(
+              (key) => lowerCaseInstruction.includes(key)
+            )
+              ? iconMappings[
+                  Object.keys(iconMappings).find((key) =>
+                    lowerCaseInstruction.includes(key)
+                  )!
+                ]
               : null;
-        return (
-          <View key={index} style={ DirectionStepsStyle.instructionsList}>
-            <View>
-              <View style={ DirectionStepsStyle.iconsBox}>
-                {instructionsIconsDisplay && (
-                  <MaterialIcons
-                    name={instructionsIconsDisplay as keyof typeof MaterialIcons.glyphMap}
-                    size={30}
-                    color="black"
-                  />
-              )}
+            return (
+              <View key={index} style={DirectionStepsStyle.instructionsList}>
+                <View>
+                  <View style={DirectionStepsStyle.iconsBox}>
+                    {instructionsIconsDisplay && (
+                      <MaterialIcons
+                        name={
+                          instructionsIconsDisplay as keyof typeof MaterialIcons.glyphMap
+                        }
+                        size={30}
+                        color="black"
+                      />
+                    )}
+                  </View>
+                </View>
+                <View style={DirectionStepsStyle.topBorder}>
+                  <Text style={DirectionStepsStyle.instructionText}>
+                    {instruction}
+                  </Text>
+                </View>
               </View>
-            </View>
-            <View style={DirectionStepsStyle.topBorder}>
-              <Text style={DirectionStepsStyle.instructionText}>
-                {instruction}
-              </Text>
-            </View>
-          </View>
-        );
-          
+            );
           })}
-          </ScrollView>
-        </View>
-    )
-  }
+      </ScrollView>
+    </View>
+  );
+};
 
 export default DirectionsSteps;
