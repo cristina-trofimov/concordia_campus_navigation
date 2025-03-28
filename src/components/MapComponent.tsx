49b22cf85c92ec2b209b528038405fe5a118436a
j-ackamir <ja_amir@live.concordia.ca>
2025-03-28 16:13:26 -0400

    Revert "Refactored MapComponent"
    This reverts commit de4ed862b4fe15905f94e127db51ad6c8cdf875a.

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -113,7 +113,10 @@ export default function MapComponent({
 
 
         const allPoints = Polyline.decode(route.overview_polyline.points);
-
+        const allDecoded: Coordinate[] = allPoints.map(([lat, lng]: [number, number]) => ({
+          latitude: lat,
+          longitude: lng,
+        }));
 
 
       } catch (error) {
