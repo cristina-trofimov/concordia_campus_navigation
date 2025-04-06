7665e5d03d0758dae9223d12220b19e52c00a342
OviyaSinna <oviyacu@gmail.com>
2025-04-06 14:11:13 -0400

    - SONARQUBE AGAIN

--- src/components/DirectionsSteps.tsx
+++ src/components/DirectionsSteps.tsx
@@ -53,12 +53,12 @@ const [sameBuilding, setSameBuilding] = useState(false);
       const instructionsGeneral = filteredSteps.map(
         (step: any) => {
           if (step.travel_mode === "TRANSIT" &&(step.transit_details.line?.vehicle?.name=='Bus')) {
-            const busNumber = step.transit_details.line?.short_name || step.transit_details.line?.name || "Bus number not found";
-            const busStop = step.transit_details.arrival_stop?.name || "Bus stop found";
+            const busNumber = step.transit_details.line?.short_name ?? step.transit_details.line?.name ?? "Bus number not found";
+            const busStop = step.transit_details.arrival_stop?.name ?? "Bus stop found";
             return step.html_instructions.replace(/<[^<>]*>/g, "") + `. Take Bus ${busNumber}. Get off at stop ${busStop}.`;
           }
           if (step.travel_mode === "TRANSIT" &&(step.transit_details.line?.vehicle?.name=='Subway')){
-            const metroStop = step.transit_details.arrival_stop?.name || "Metro stop not found";
+            const metroStop = step.transit_details.arrival_stop?.name ?? "Metro stop not found";
             return step.html_instructions.replace(/<[^<>]*>/g, "") + `. Get off at stop ${metroStop}.`;
           }
           return step.html_instructions.replace(/<[^<>]*>/g, "");
